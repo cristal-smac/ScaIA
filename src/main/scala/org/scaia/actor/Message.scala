bec69cae565ada26efd1ebaa9dd4e19627d11e9e
morge <Maxime.Morge@univ-lille1.fr>
2017-12-10 20:39:12 +0100

    Clean messages

--- src/main/scala/org/scaia/actor/Message.scala
+++ src/main/scala/org/scaia/actor/Message.scala
@@ -14,13 +14,14 @@ case class Result(matching: Matching) extends  Message// The solver returns a ma
 case class Inform(addresses :  Map[String, ActorRef]) extends Message// Send a white page
 case object Stop extends Message//Stop an agent
 
-case object Confirm extends Message// Confirm the reception of a message
-
+case object Confirm extends Message// Confirm the exclusion
 case class Assignement(i: String, a: String) extends Message// The assignement of i with a is reported
 case class Disassignement(i: String) extends Message// The disassignement of i is reported
+
 case class Propose(i: String) extends Message//i makes a proposal
 case object Accept extends  Message// Accept a proposal
 case object Reject extends Message// Reject a proposal
 case object Withdraw extends Message// Fire an individual
+
 case class Query(g: Set[String], a: String) extends Message// Ask for an utility to be in g for a
 case class Reply(g: Set[String], a: String, utility: Double) extends Message// Reply with the utility to be in g for a
