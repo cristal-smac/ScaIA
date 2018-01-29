09900b94088f5d46c6b6aea1238979dd0fad8b8a
morge <Maxime.Morge@univ-lille1.fr>
2018-01-29 08:40:21 +0100

    Time Ut exp ready

--- runExperiments.sh
+++ runExperiments.sh
@@ -1,4 +1,4 @@
 # Copyright (C) Maxime MORGE 2017
-# depreciated
-sbt "run Welfare Utilitarian" 2>&1 | tee -a experiments/data/welfareUtilitarian.csv
+#sbt "run Welfare Utilitarian" 2>&1 | tee -a experiments/data/welfareUtilitarian.csv
+sbt "run Time Utilitarian" 2>&1 | tee -a experiments/data/timeUtilitarian.csv
 
