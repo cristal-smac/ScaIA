90e4ae24de849733ec3809759dc008428f2b801a
morge <Maxime.Morge@univ-lille1.fr>
2018-01-30 14:31:40 +0100

    runExp Egal Wel

--- runExperiments.sh
+++ runExperiments.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 # Copyright (C) Maxime MORGE 2017
 #sbt "run Welfare Utilitarian" 2>&1 | tee -a experiments/data/welfareUtilitarian.csv
-sbt "run Time Utilitarian" 2>&1 | tee -a experiments/data/timeUtilitarian.csv
-
+#sbt "run Time Utilitarian" 2>&1 | tee -a experiments/data/timeUtilitarian.csv
+sbt "run Welfare Egalitarian" 2>&1 | tee -a experiments/data/welfareEgalitarian.csv
