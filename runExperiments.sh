bdc98418797ebc759c95931bd07c511d4241b03e
morge <Maxime.Morge@univ-lille1.fr>
2018-02-02 20:51:45 +0100

    Update expermimentation rum

--- runExperiments.sh
+++ runExperiments.sh
@@ -2,4 +2,5 @@
 # Copyright (C) Maxime MORGE 2017
 #sbt "run Welfare Utilitarian" 2>&1 | tee -a experiments/data/welfareUtilitarian.csv
 #sbt "run Time Utilitarian" 2>&1 | tee -a experiments/data/timeUtilitarian.csv
-sbt "run Welfare Egalitarian" 2>&1 | tee -a experiments/data/welfareEgalitarian.csv
+#sbt "run Welfare Egalitarian" 2>&1 | tee -a experiments/data/welfareEgalitarian.csv
+sbt "run Time Egalitarian" 2>&1 | tee -a experiments/data/timeEgalitarian.csv
